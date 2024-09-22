263d75ed519464936c832d034de554ba20f87e41
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-Air.fios-router.home>
2024-09-22 08:42:55 -0400

    basic calc

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,3 +1,15 @@
 num1 = int(input("enter a number: "))
 oper = input("select + , - , * or /: ")
 num2 = int(input("enter a number: "))
+
+
+if oper == "+":
+   print(num1 + num2)
+elif oper == "-":
+   print(num1 - num2)
+elif oper == "*":
+   print(num1 * num2)
+elif oper == "/":
+   print(num1 / num2)
+else:
+   print('error')
